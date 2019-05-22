186256ee673cb1456470e095a567722ec4f2921f
Harry Ganz <hganz@harryganz.com>
2019-05-21 20:34:16 -0400

    Creates FileDictionary

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -1,13 +1,27 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
 export interface Dictionary {
     getWord : () => string
 }
 
-export class FileDictionary {
-    fileName : string;
+export class FileDictionary implements Dictionary {
+    private corpus : {  [propname : string]: any }
 
-    constructor(fileName : string) {
-        this.fileName = fileName;
+    constructor() {
+        this.corpus = {};
     }
 
+    loadDictionary(fileName: string) : void {
+        try {
+            this.corpus = JSON.parse(readFileSync(resolve(__dirname, fileName), { encoding: 'UTF-8' }));
+        } catch (e) {
+            throw new Error('could not load dictionary file: ' + e);
+        }
+    }
 
+    getWord() : string {
+        let n = this.corpus.length;
+        return this.corpus[Math.floor(Math.random() * n)];
+    }
 }
